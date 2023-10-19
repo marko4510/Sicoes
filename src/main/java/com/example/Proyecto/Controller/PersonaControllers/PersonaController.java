54a4203821fb43ee424c9062365a47c098d59f8b
marko4510 <marcocuellar7293@gmail.com>
2023-10-19 10:45:19 -0400

    aasdfgfds

---
+++ src/main/java/com/example/Proyecto/Controller/PersonaControllers/PersonaController.java
@@ -0,0 +1,133 @@
+package com.example.Proyecto.Controller.PersonaControllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+import com.example.Proyecto.Models.Entity.Persona;
+import com.example.Proyecto.Models.Entity.Usuario;
+import com.example.Proyecto.Models.IService.IPersonaService;
+import com.example.Proyecto.Models.IService.IUsuarioService;
+
+
+@Controller
+public class PersonaController {
+
+    @Autowired
+    private IPersonaService personaService;
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+  
+
+    @RequestMapping(value = "PersonaR", method = RequestMethod.GET)
+    public String PersonaR(@Validated Persona persona, Model model, HttpServletRequest request) throws Exception {
+
+        if (request.getSession().getAttribute("persona") != null) {
+            List<Persona> personas = personaService.findAll();
+           
+           
+            model.addAttribute("personas", personas);
+        
+
+            return "persona/persona-adm";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+  
+
+    @RequestMapping(value = "PersonaF", method = RequestMethod.POST)
+    public String PersonaF(HttpServletRequest request, @Validated Persona persona) { 
+
+
+        persona.setEstado_persona("A");
+        personaService.save(persona);
+
+
+        return "redirect:/PersonaR";
+    }
+    
+
+   @RequestMapping(value = "/editar-persona/{id_persona}")
+    public String editar_c(@PathVariable("id_persona") String id_persona, Model model, HttpServletRequest request) {
+        if (request.getSession().getAttribute("persona") != null) {
+            try {
+                Long id_per = Long.parseLong(id_persona);
+                Persona persona = personaService.findOne(id_per);
+                model.addAttribute("persona", persona);
+
+                List<Persona> personas = personaService.findAll();
+               
+                model.addAttribute("personas", personas);
+  
+                return "persona/persona-adm";
+
+            } catch (Exception e) {
+
+                return "redirect:/AdmPG";
+            }
+        } else {
+            return "redirect:/";
+        }
+
+    }
+
+    @RequestMapping(value = "/PersonaModF", method = RequestMethod.POST) // Enviar datos de Registro a Lista
+    public String tpconvenio_mod(HttpServletRequest request, @Validated Persona persona,
+            RedirectAttributes redirectAttrs) { // validar los datos capturados (1)
+
+        //Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+
+        persona.setEstado_persona("A");
+        personaService.save(persona);
+        return "redirect:/PersonaR";
+    }
+
+    @RequestMapping(value = "/eliminar-persona/{id_persona}")
+    public String eliminar_c(HttpServletRequest request, @PathVariable("id_persona") String id_persona)
+            throws Exception {
+        if (request.getSession().getAttribute("persona") != null) {
+        try {
+            Long id_per = Long.parseLong(id_persona);
+            Persona persona = personaService.findOne(id_per);
+            Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+            persona.setEstado_persona("X");
+            personaService.save(persona);
+            return "redirect:/PersonaR";
+        } catch (Exception e) {
+            return "redirect:/AdmPG";
+        }
+        } else {
+            return "redirect:/";
+        }
+    }
+    
+
+    @GetMapping("/tablePersonas")
+    public String tablePersonas(@Validated Persona persona, Model model) throws Exception {
+
+        List<Persona> personas = personaService.findAll();
+ 
+        model.addAttribute("personas", personas);
+
+
+        return "persona/tableFragmentPer :: table";
+    }
+    
+
+}
---
+++ src/main/java/com/example/Proyecto/Controller/UsuarioControllers/UsuarioController.java
@@ -0,0 +1,132 @@
+package com.example.Proyecto.Controller.UsuarioControllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.Proyecto.Models.Entity.Persona;
+import com.example.Proyecto.Models.Entity.Usuario;
+import com.example.Proyecto.Models.IService.IPersonaService;
+import com.example.Proyecto.Models.IService.IUsuarioService;
+
+@Controller
+public class UsuarioController {
+    
+     @Autowired
+    private IPersonaService personaService;
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+  
+
+    @RequestMapping(value = "UsuarioR", method = RequestMethod.GET)
+    public String UsuarioR(@Validated Usuario usuario, Model model, HttpServletRequest request) throws Exception {
+
+        if (request.getSession().getAttribute("persona") != null) {
+            List<Persona> personas = personaService.findAll();
+             List<Usuario> usuarios = usuarioService.findAll();
+           
+           
+            model.addAttribute("personas", personas);
+            model.addAttribute("usuarios", usuarios);
+        
+
+            return "usuario/usuario-adm";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @RequestMapping(value = "UsuarioF", method = RequestMethod.POST)
+    public String UsuarioF(HttpServletRequest request, @Validated Usuario usuario) { 
+
+        usuario.setEstado("A");
+        usuarioService.save(usuario);
+
+
+
+        return "redirect:/UsuarioR";
+    }
+
+     @RequestMapping(value = "/editar-usuario/{id_usuario}")
+    public String editar_usu(@PathVariable("id_usuario") String id_usuario, Model model, HttpServletRequest request) {
+        if (request.getSession().getAttribute("persona") != null) {
+            try {
+                Long id_usu = Long.parseLong(id_usuario);
+                Usuario usuario = usuarioService.findOne(id_usu);
+                model.addAttribute("usuario", usuario);
+
+            List<Persona> personas = personaService.findAll();
+             List<Usuario> usuarios = usuarioService.findAll();
+               
+                model.addAttribute("personas", personas);
+                model.addAttribute("usuarios", usuarios);
+                return "usuario/usuario-adm";
+
+            } catch (Exception e) {
+
+                return "redirect:/AdmPG";
+            }
+        } else {
+            return "redirect:/";
+        }
+
+    }
+
+
+    @RequestMapping(value = "/UsuarioModF", method = RequestMethod.POST) // Enviar datos de Registro a Lista
+    public String usuario_mod(HttpServletRequest request, @Validated Usuario usuario,
+            RedirectAttributes redirectAttrs) { // validar los datos capturados (1)
+
+
+        
+        usuario.setEstado("A");
+        usuarioService.save(usuario);
+        return "redirect:/UsuarioR";
+    }
+
+    @RequestMapping(value = "/eliminar-usuario/{id_usuario}")
+    public String eliminar_usuario(HttpServletRequest request, @PathVariable("id_usaurio") String id_usuario)
+            throws Exception {
+        if (request.getSession().getAttribute("persona") != null) {
+        try {
+            Long id_usu = Long.parseLong(id_usuario);
+            Usuario usuario = usuarioService.findOne(id_usu);
+            usuario.setEstado("X");
+            usuario.setContrasena(usuario.getContrasena());
+            usuarioService.save(usuario);
+            
+        
+            return "redirect:/UsuarioR";
+        } catch (Exception e) {
+            return "redirect:/AdmPG";
+        }
+        } else {
+            return "redirect:/";
+        }
+    }
+
+
+    @GetMapping("/tableUsuarios")
+    public String tableUsuarios(@Validated Usuario usuario, Model model) throws Exception {
+
+        List<Usuario> usuarios = usuarioService.findAll();
+ 
+        model.addAttribute("usuarios", usuarios);
+
+
+        return "usuario/tableFragment :: table";
+    }
+
+}
