43c53e3d0b86ef6803326dbe8331438150829b2c
marcocuellar7293@gmail.com <marcocuellar7293@gmail.com>
2024-06-14 17:12:30 -0400

    asdasd

--- src/main/java/com/example/Proyecto/Models/Dao/IUsuarioDao.java
+++ src/main/java/com/example/Proyecto/Models/Dao/IUsuarioDao.java
@@ -1,16 +1,18 @@
 package com.example.Proyecto.Models.Dao;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Proyecto.Models.Entity.Usuario;
 
 public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
 
-    @Query(value = "select * from insertar_adm(?1, ?2, ?3, ?4)", nativeQuery = true)
+
+    @Procedure(procedureName = "insertar_adm")
     public Long insertar_adm(String usuario_nom, String contrasena, Integer id_persona, Integer id_consejo);
 
-    @Query(value = "select * from validar_adm(?1, ?2)", nativeQuery = true)
+    @Procedure(procedureName = "validar_adm")
     public Long validar_adm(String usuario_nom, String contrasena);
 
     @Query(value = "select * from usuario u where u.usuario_nom = ?1 and u.usuario_codigo = ?2", nativeQuery = true)
